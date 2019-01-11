05b2a5a72cb19fa064b9539695c9d1cd6c5b0ade
monggopesen <monggopesen@monggopesens-MacBook-Air.local>
2019-01-11 06:17:22 -0800

    position kementrian

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ export const center = { x: width / 2, y: 840 / 2 }
 export const yearCenters = {
   "Kementerian Sumber Manusia": { x: width / 4, y: 200, yTitle: 10, xTitle: 170 },
   "Kementerian Perumahan Dan Kerajaan Tempatan": { x: width /2 , y: 200,yTitle: 150, xTitle: 490 },
-  "Kementerian Dalam Negeri":{ x: (3 / 4) * width, y: 200,yTitle: 0, xTitle: 800},
+  "Kementerian Dalam Negeri":{ x: (3 / 4) * width, y: 200,yTitle: 0, xTitle: 824},
   
 
   "Kementerian  Pembangunan Luar Bandar":{ x: width / 4, y: 400,yTitle: 200 , xTitle: 160 },
@@ -15,7 +15,7 @@ export const yearCenters = {
 
   "Kementerian Kerja Raya":{ x: width /4 , y: 600,yTitle: 450, xTitle: 150 },
   "Kementerian Pengangkutan":{ x: width / 2, y: 600,yTitle: 550 , xTitle: 500 },
-  "Kementerian Tenaga, Sains, Teknologi, Alam Sekitar Dan Perubahan Iklim": { x: (3/4) *  width, y: 600,yTitle: 440 , xTitle: 850 },
+  "Kementerian Tenaga, Sains, Teknologi, Alam Sekitar Dan Perubahan Iklim": { x: (3/4) *  width, y: 700,yTitle: 440 , xTitle: 850 },
 
   "Kementerian Pembangunan Wanita, Keluarga dan Masyarakat":{ x: width / 4, y: 800,yTitle: 660 , xTitle: 130 },
   "Kementerian Pendidikan":{ x: width /2  , y: 800,yTitle: 960 , xTitle: 500 },
@@ -23,7 +23,7 @@ export const yearCenters = {
 
   "Kementerian Luar Negeri":{ x: width / 4, y: 1000,yTitle: 950 , xTitle: 150 },
   "Kementerian Industri Utama":{ x: width / 2, y: 1000,yTitle: 1100 , xTitle: 480 },
-  "Kementerian Perdagangan Dalam Negeri Dan Hal Ehwal Pengguna":{ x: (3 / 4) * width, y: 1000,yTitle: 890  , xTitle: 875 },
+  "Kementerian Perdagangan Dalam Negeri Dan Hal Ehwal Pengguna":{ x: (3 / 4) * width, y: 1000,yTitle: 855  , xTitle: 868 },
 
   "Kementerian Pelancongan, Seni Dan Budaya":{ x: width / 4, y: 1200,yTitle: 1090  , xTitle: 150 },
   "Kementerian Wilayah Persekutuan":{ x:  width / 2, y: 1200,yTitle: 1250  , xTitle: 480 },
@@ -35,7 +35,7 @@ export const yearCenters = {
 
   "Kementerian Pertanian dan Industri Asas Tani":{ x: width / 4, y: 1600,yTitle: 1500 , xTitle: 150 },
   "Kementerian Kesihatan":{ x: width /2, y: 1600,yTitle: 1800, xTitle: 470 },
-  "Kementerian Kewangan":{ x: (3/4) * width, y: 1600,yTitle: 1500, xTitle: 875 },
+  "Kementerian Kewangan":{ x: (3/4) * width, y: 1600,yTitle: 1500, xTitle: 823 },
 
   "Kementerian Pembangunan Usahawan":{ x: width /4, y: 1810,yTitle: 1800, xTitle: 150 },
   
